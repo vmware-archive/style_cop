371314c3439f2159d3ec35c38d0283df0aa6eea6
David Dening and David Edwards <style_cop+pair+ddening+dedwards@pivotallabs.com>
2014-05-20 17:31:29 -0400

    Create ApplicableStyleHash

--- lib/style_cop/selector.rb
+++ lib/style_cop/selector.rb
@@ -26,10 +26,10 @@ module StyleCop
 
     def rule_based_representation
       clean_key = key.gsub(".style-cop-pattern", "")
-      return { clean_key => applicable_css } if children.empty?
-      children_hash = children.map(&:representation).inject({}) { |hash, h| hash.merge!(h) }
+      return { clean_key => ApplicableStylesHash.new(session, key, selector.path) } if children.empty?
+      children_hash = children.map(&:rule_based_representation).inject({}) { |hash, h| hash.merge!(h) }
       Hash[children_hash.map { |key, value| ["#{clean_key} #{key}", value] }].merge(
-        clean_key => applicable_css
+        clean_key => ApplicableStylesHash.new(session, key, selector.path)
       )
     end
 
@@ -47,54 +47,23 @@ module StyleCop
       selector.session
     end
 
-
-    def applicable_css
-      ComputedStylesHash.new(session, key, selector.path).select do |key, value|
-        relevant_attributes.include? key
-      end
-    end
-
-    def relevant_attributes
-      return @relevant_attributes if @relevant_attributes
-
-      @relevant_attributes = []
-      applicable_style = session.evaluate_script(applicable_style_script) 
-
-      if applicable_style && !applicable_style.empty?
-        (0...applicable_style['length']).each do |i|
-          rule = applicable_style[i.to_s]['style']
-          (0...rule['length']).each do |j|
-            @relevant_attributes << rule[j.to_s]
-          end
-        end
-      else
-        return []
-      end
-      @relevant_attributes = @relevant_attributes.uniq
-    end
-
-    def applicable_style_script
-      %{
-        var node = document.evaluate("/#{selector.path}",
-            document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue
-        window.getMatchedCSSRules(node);
-      }
-    end
-
     class ComputedStylesHash < Hash
 
       def initialize(session, key, path)
         @session = session
         @key = key
         @path = path
+        build_hash
+      end
 
+      private
+
+      def build_hash
         attribute_value_pairs = computed_css.split(/\s*;\s*/).map { |s| s.split(/\s*:\s*/) }
         attribute_value_pairs.each do |attribute, value|
           self[attribute] = value
         end
       end
-
-      private
 
       def computed_css
         if computed_style = @session.evaluate_script(computed_style_script)
@@ -109,6 +78,45 @@ module StyleCop
         var node = document.evaluate("/#{@path}",
             document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue
         window.getComputedStyle(node);
+        }
+      end
+    end
+
+    class ApplicableStylesHash < ComputedStylesHash
+
+      private
+
+      def build_hash
+        super
+        self.select! do |key, value|
+          relevant_attributes.include? key
+        end
+      end
+
+      def relevant_attributes
+        return @relevant_attributes if @relevant_attributes
+
+        @relevant_attributes = []
+        applicable_style = @session.evaluate_script(applicable_style_script) 
+
+        if applicable_style && !applicable_style.empty?
+          (0...applicable_style['length']).each do |i|
+            rule = applicable_style[i.to_s]['style']
+            (0...rule['length']).each do |j|
+              @relevant_attributes << rule[j.to_s]
+            end
+          end
+        else
+          return []
+        end
+        @relevant_attributes = @relevant_attributes.uniq
+      end
+
+      def applicable_style_script
+        %{
+          var node = document.evaluate("/#{@path}",
+              document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue
+          window.getMatchedCSSRules(node);
         }
       end
     end
