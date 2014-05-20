3d488905a46e0842bcbefc116e46106ea0f49560
David Dening and David Edwards <style_cop+pair+ddening+dedwards@pivotallabs.com>
2014-05-20 17:23:11 -0400

    Extracted ComputedStylesHash, relevant_attributes

--- lib/style_cop/selector.rb
+++ lib/style_cop/selector.rb
@@ -17,10 +17,10 @@ module StyleCop
 
     def representation
       clean_key = key.gsub(".style-cop-pattern", "")
-      return { clean_key => computed_style_hash } if children.empty?
+      return { clean_key => ComputedStylesHash.new(session, key, selector.path) } if children.empty?
       children_hash = children.map(&:representation).inject({}) { |hash, h| hash.merge!(h) }
       Hash[children_hash.map { |key, value| ["#{clean_key} #{key}", value] }].merge(
-        clean_key => computed_style_hash
+        clean_key => ComputedStylesHash.new(session, key, selector.path)
       )
     end
 
@@ -43,47 +43,34 @@ module StyleCop
       end
     end
 
-    def computed_style_hash
-      Hash[computed_css.split(/\s*;\s*/).map { |s| s.split(/\s*:\s*/) }]
-    end
-
-    def computed_css
-      if computed_style = session.evaluate_script(computed_style_script)
-        computed_style["cssText"]
-      else
-        raise RuntimeError.new("Can't find css for #{selector.key}")
-      end
-    end
-
     def session
       selector.session
     end
 
-    def computed_style_script
-      %{
-        var node = document.evaluate("/#{selector.path}",
-            document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue
-        window.getComputedStyle(node);
-      }
-    end
 
     def applicable_css
-      relevant_attributes = []
+      ComputedStylesHash.new(session, key, selector.path).select do |key, value|
+        relevant_attributes.include? key
+      end
+    end
+
+    def relevant_attributes
+      return @relevant_attributes if @relevant_attributes
+
+      @relevant_attributes = []
       applicable_style = session.evaluate_script(applicable_style_script) 
 
       if applicable_style && !applicable_style.empty?
         (0...applicable_style['length']).each do |i|
           rule = applicable_style[i.to_s]['style']
           (0...rule['length']).each do |j|
-            relevant_attributes << rule[j.to_s]
+            @relevant_attributes << rule[j.to_s]
           end
         end
       else
-        return {}
+        return []
       end
-      relevant_attributes.uniq!
-
-      computed_style_hash.select { |key, value| relevant_attributes.include? key }
+      @relevant_attributes = @relevant_attributes.uniq
     end
 
     def applicable_style_script
@@ -92,6 +79,38 @@ module StyleCop
             document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue
         window.getMatchedCSSRules(node);
       }
+    end
+
+    class ComputedStylesHash < Hash
+
+      def initialize(session, key, path)
+        @session = session
+        @key = key
+        @path = path
+
+        attribute_value_pairs = computed_css.split(/\s*;\s*/).map { |s| s.split(/\s*:\s*/) }
+        attribute_value_pairs.each do |attribute, value|
+          self[attribute] = value
+        end
+      end
+
+      private
+
+      def computed_css
+        if computed_style = @session.evaluate_script(computed_style_script)
+          computed_style["cssText"]
+        else
+          raise RuntimeError.new("Can't find css for #{@key}")
+        end
+      end
+
+      def computed_style_script
+        %{
+        var node = document.evaluate("/#{@path}",
+            document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue
+        window.getComputedStyle(node);
+        }
+      end
     end
   end
 end
